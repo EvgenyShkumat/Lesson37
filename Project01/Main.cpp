3209d605fd756fdb704c0e49f70545900e260261
EvgenyShkumat <157722237+EvgenyShkumat@users.noreply.github.com>
2024-02-01 18:35:48 +0300

    Update Main.cpp

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #define N 0
 using namespace std;
-int sum(int a, int b); 
+int sum(int a, int b, int c); 
 
 int main() {
 
-	cout << sum(107, 891);
+	cout << sum(107, 891, 523);
 
 	return 0;
 }
